266ff213ecc88186a371d2be042345a026edd5ae
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-05 14:59:23 -0300

    fixed create root issue

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Welcome to Groove Store!</h1>
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
