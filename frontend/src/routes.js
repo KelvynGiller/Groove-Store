92f3ddc4c14b86490deff54b9402c7cc846964c4
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-05 13:10:26 -0300

    created components and pages files

---
+++ frontend/src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const appRoutes = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route />
+                <Route />
+                <Route />
+                <Route />
+                <Route />
+            </Routes>
+        </Router>
+    )
+}
+
+export default appRoutes
