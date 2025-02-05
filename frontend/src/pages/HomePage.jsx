95b21f69f741f2e4a925287a2d6c2eaee93b0be7
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-05 17:35:11 -0300

    updated home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Header from '../components/Header';
 import Home from '../components/Home'
 import Footer from '../components/Footer';
+import ProductList from "../components/ProductList";
 
 const HomePage = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/products") 
+          .then(response => setProducts(response.data))
+          .catch(error => console.error("Error fetching products:", error));
+      }, []);
+
+
   return (
     <div>
       <Header />
       <Home />
+      <ProductList products={products} />
       <Footer />
     </div>
   );
