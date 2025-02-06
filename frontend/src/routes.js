6e41c54556af6a9cd3dbbcff51f2fa4e8c67e17e
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-06 10:49:09 -0300

    updated routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import ProductListPage from './pages/ProductListPage';
+import ProductDetailPage from './pages/ProductDetailPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
 
 const AppRoutes = () => {
     return (
@@ -11,6 +15,10 @@ const AppRoutes = () => {
                 <Route path="/" element={<HomePage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route path="/login" element={<LoginPage />} />
+                <Route path="/products" element={<ProductListPage />} />
+                <Route path="/product/:id" element={<ProductDetailPage />} />
+                <Route path="/cart" element={<CartPage />} />
+                <Route path="/checkout" element={<CheckoutPage />} />
             </Routes>
         </Router>
     )
