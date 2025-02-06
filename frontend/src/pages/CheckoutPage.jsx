dbc97a6056ea5ddfa432c83aff65974a35a88cce
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-06 10:44:29 -0300

    created checkout page

---
+++ frontend/src/pages/CheckoutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const CheckoutPage = () => {
+    return (
+        <div>
+            <h1>
+                Checkout
+            </h1>
+            <p>Checkout process under construction...</p>
+        </div>
+    );
+};
+
+export default CheckoutPage;
