6a82fb65cf6671d84ef11876f892597846ce7a2d
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-13 12:58:20 -0300

    header enabled to use redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchCart } from './slices/CartSlice';
+import { auth } from './utils/firebase';
 import AppRoutes from './routes';
 
 const App = () => {
-  return (
-    <div>
-      <AppRoutes />
-    </div>
-  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        dispatch(fetchCart(user.uid));
+      }
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
+  return <AppRoutes />;
 };
 
 export default App;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { useCart } from "../context/CartContext";
 import { FiShoppingCart } from "react-icons/fi";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const [user, setUser] = useState(null);
-  const { cart } = useCart();
+  const cart = useSelector((state) => state.cart.items || []);
 
   useEffect(() => {
     const auth = getAuth();
@@ -14,29 +14,16 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
-  const handleLogout = () => {
-    const auth = getAuth();
-    signOut(auth);
-  };
-
   return (
     <header className="w-full h-[101px] bg-black flex items-center justify-between px-8 fixed top-0 left-0 z-10">
       <h1 className="text-white text-[20px] font-normal leading-[24px] font-[Goldman]">
         Groove Store
       </h1>
       <nav className="flex space-x-6">
-        <Link to="/" className="text-white text-[14px] font-[Golos Text]">
-          Home
-        </Link>
-        <Link to="/products" className="text-white text-[14px] font-[Golos Text]">
-          Tracks
-        </Link>
-        <Link to="/genres" className="text-white text-[14px] font-[Golos Text]">
-          Genres
-        </Link>
-        <Link to="/artists" className="text-white text-[14px] font-[Golos Text]">
-          Artists
-        </Link>
+        <Link to="/" className="text-white text-[14px] font-[Golos Text]">Home</Link>
+        <Link to="/products" className="text-white text-[14px] font-[Golos Text]">Tracks</Link>
+        <Link to="/genres" className="text-white text-[14px] font-[Golos Text]">Genres</Link>
+        <Link to="/artists" className="text-white text-[14px] font-[Golos Text]">Artists</Link>
       </nav>
       <div className="flex items-center space-x-6">
         {user ? (
@@ -49,23 +36,20 @@ const Header = () => {
                 </span>
               )}
             </Link>
-            <button onClick={handleLogout} className="text-white text-[18px] font-[Goldman]">
+            <button onClick={() => signOut(getAuth())} className="text-white text-[18px] font-[Goldman]">
               Logout
             </button>
           </>
         ) : (
           <>
-            <Link to="/login" className="text-white text-[18px] font-[Goldman]">
-              Login
-            </Link>
-            <Link to="/register" className="text-white text-[18px] font-[Goldman]">
-              Register
-            </Link>
+            <Link to="/login" className="text-white text-[18px] font-[Goldman]">Login</Link>
+            <Link to="/register" className="text-white text-[18px] font-[Goldman]">Register</Link>
           </>
         )}
       </div>
     </header>
   );
 };
+
 
 export default Header;
