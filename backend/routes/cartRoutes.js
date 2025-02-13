fc9ed2d7bf874a9c7250ca6f8d04313118d2f5f2
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-13 12:33:32 -0300

    added verify token to cart routes

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const cartController = require('../controllers/cartController');
-const orderController = require('../controllers/orderController');
+const verifyToken = require('../firebaseAdmin');
 
-router.post('/create-or-get', cartController.createOrGetCart);
-router.post('/', cartController.createCart);
-router.post('/:cart_id/add-product', cartController.addProductToCart);
-router.get('/:cart_id/items', cartController.getCartItems);
-router.put('/:cart_id/update-quantity', cartController.updateProductQuantity);
-router.delete('/:cart_id/remove-product', cartController.removeProductFromCart);
+router.post('/create-or-get', verifyToken, cartController.createOrGetCart);
+router.post('/', verifyToken, cartController.createCart);
+router.post('/:cart_id/add-product', verifyToken, cartController.addProductToCart);
+router.get('/:cart_id/items', verifyToken, cartController.getCartItems); 
+router.put('/:cart_id/update-quantity', verifyToken, cartController.updateProductQuantity); 
+router.delete('/:cart_id/remove-product', verifyToken, cartController.removeProductFromCart); 
 
 module.exports = router;
