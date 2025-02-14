8b5a4962075d71a540b061a567d46955046be7d3
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-14 11:42:16 -0300

    added checkout slice to redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './slices/CartSlice';
+import checkoutReducer from './slices/CheckoutSlice'
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    checkout: checkoutReducer,
   },
 });
 
