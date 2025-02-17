b75fb59f0c353f4305b7f64721bd7dd217983898
Kelvyn Giller <kelvyngiller9@gmail.com>
2025-02-17 15:36:20 -0300

    added success page to routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -8,6 +8,8 @@ import ProductDetailPage from './pages/ProductDetailPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
 import Dashboard from './pages/DashboardPage';
+import SuccessPage from './pages/SuccessPage';
+import OrderHistory from './pages/OrderHistoryPage';
 
 
 const AppRoutes = () => {
@@ -22,6 +24,7 @@ const AppRoutes = () => {
           <Route path="/product/:id" element={<ProductDetailPage />} />
           <Route path="/cart" element={<CartPage />} />
           <Route path="/checkout/:orderId" element={<CheckoutPage />} />
+          <Route path="/success" element={<SuccessPage/>}/>
         </Routes>
       </Router>
   );
